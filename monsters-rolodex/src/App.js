16c39322a7dbeffb058b403f4e24100964789762
Paul Baur <paulbaur@gmx.at>
2020-06-25 12:08:01 +0200

    adds search & stateObjectDestructuring

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 		super(props);
 		this.state = {
 			monsters: [],
+			searchField: "",
 		};
 	}
 	componentDidMount() {
@@ -16,9 +17,19 @@ class App extends Component {
 	}
 
 	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+
 		return (
 			<div className="App">
-				<CardList monsters={this.state.monsters} />
+				<input
+					type="search"
+					placeholder="search monster"
+					onChange={(e) => this.setState({ searchField: e.target.value })}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -6,7 +6,6 @@ export const Card = (props) => (
 		<img
 			alt="monster"
 			src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
-			alt=""
 		/>
 		<h2>{props.monster.name}</h2>
 		<p>{props.monster.email}</p>
