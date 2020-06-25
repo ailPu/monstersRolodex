29a60cb6d65c22f46509284d15d7ecce2da770ac
Paul Baur <paulbaur@gmx.at>
2020-06-25 13:49:50 +0200

    Updates

---
+++ precache-manifest.184287d2bc00f958aed9f2c4c1606c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b534e92d93fc8c61cc1f5b1de9288e",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "8ec96078bbc845450bd8",
+    "url": "/monstersRolodex/static/css/main.82b800a7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstersRolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersRolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec96078bbc845450bd8",
+    "url": "/monstersRolodex/static/js/main.cbebe8a4.chunk.js"
+  },
+  {
+    "revision": "0735bc06010632169b4e",
+    "url": "/monstersRolodex/static/js/runtime-main.0dd8b790.js"
+  }
+]);
